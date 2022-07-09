e90bf6f9eaf9888a44086dd2d83740cf2763ae54
Илья <96692982+unrea1projects@users.noreply.github.com>
2022-07-09 16:42:41 +0300

    Update base.py
    Model bag fixes

--- database/base.py
+++ database/base.py
@@ -1,5 +1,8 @@
 import sqlite3
+from pydantic import BaseModel as PydancitBaseModel
+
 from core import config
+
 
 def dict_factory(cursor, row):
     d = {}
@@ -10,3 +13,18 @@ def dict_factory(cursor, row):
 connection = sqlite3.connect(config.database.path, check_same_thread = False)
 connection.row_factory = dict_factory
 cursor = connection.cursor()
+
+class BaseModel(PydancitBaseModel):
+    id : int
+    
+    def create(self):
+        raise NotImplementedError()
+
+    def get(self):
+        raise NotImplementedError()
+
+    def update(self):
+        raise NotImplementedError()
+
+    def delete(self):
+        raise NotImplementedError()
