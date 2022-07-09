4221c6b3cd8859fb27a2362068bc70361a0e022e
Илья <96692982+unrea1projects@users.noreply.github.com>
2022-07-09 16:42:11 +0300

    Update functions.py
    Models bag fix

--- handlers/user/functions.py
+++ handlers/user/functions.py
@@ -7,9 +7,13 @@ from bot import bot
 
 async def process_send_welcome(message : types.Message) -> None:
     user_id = message.from_user.id
-    user = User.create(user_id = user_id)
 
-    await bot.send_message(chat_id = user_id, text = user.id)
+    user = User.get(user_id = user_id)
+    if not user:
+        user = User(user_id = user_id)
+        user.create()
+
+    await bot.send_message(chat_id = user_id, text = user.user_id)
 
 def install(_dispatcher : aiogram.Dispatcher) -> None:
     _dispatcher.register_message_handler(callback = process_send_welcome, commands = ['start', 'help'])
